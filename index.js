7ddec4ac0574dbe472c57c2dfa93e01048cd3b66
Joni Honkanen <joni.honkanen@tuni.fi>
2021-12-03 14:32:48 +0200

    Add database connection to index.js

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const app = express();
 var cors = require('cors');
 app.use(cors());
 
+let config = {
+  host: 'mydb.tamk.fi',
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
 const port = process.env.PORT || 8080;
 
+/*
 const db = [{ name: 'tiina' }, { name: 'jack' }];
 
 app.get('/names', (req, res) => {
   res.send(db);
+});
+*/
+
+var pool = mysql.createPool(config);
+app.get('/', (req, res) => {
+  pool.query('SELECT * from location', (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
